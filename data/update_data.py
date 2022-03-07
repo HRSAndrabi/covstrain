182ca238ac14500730410d40cf463abad2f288e5
Hassan Andrabi <handrabi53@gmail.com>
2022-03-08 01:47:30 +1100

    Create GISAID data management module.

--- data/update_data.py
+++ data/update_data.py
@@ -2,17 +2,40 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
+def request_data():
+    print("Requesting data ...")
+    url = 'https://www.epicov.org/epi3/feed/gisaid_variants_statistics.json'
+    target_path = './data/gisaid/raw.json'
 
-url = 'https://www.epicov.org/epi3/feed/gisaid_variants_statistics.json'
-target_path = './gisaid_variants_statistics2.json'
+    response = requests.get(url, stream=True, auth=HTTPBasicAuth('USERNAME', 'PASSWORD'))
+    if response.status_code == 200:
+        print("Request OK.")
+        with open(target_path, 'w') as f:
+            print("Writing data ...")
+            json.dump(response.json(), f)
+            print(f"Data written to: {target_path}")
 
-response = requests.get(url, stream=True, auth=HTTPBasicAuth('USER_NAME', 'PASSWORD'))
-print(response.json())
-if response.status_code == 200:
-    with open(target_path, 'w') as f:
-        json.dump(response.json(), f)
+def make_country_output(country):
+    gisaid_data = json.load(open("./data/gisaid/raw.json"))
+    output = gisaid_data["stats"]
+    output_path = f"data/gisaid/{country['cca3'].lower()}.json"
+    for date, variant_data in output.items():
+        if country["name"]["common"] in variant_data:
+            output[date] = variant_data[country["name"]["common"]]
+        else:
+            output[date] = {}
 
-print("done")
+    with open(output_path, 'w') as f:
+        json.dump(output, f, indent=4)
+
+def clean_data():
+    country_data = json.load(open("./data/countries.json"))
+    for country in country_data:
+        print(country["name"]["common"])
+        make_country_output(country=country)
+        
+
+clean_data()
 
 
 
