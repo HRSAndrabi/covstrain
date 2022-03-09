8eb13faaf359dfde5991609ee72dad37c0ae5524
Hassan Andrabi <handrabi53@gmail.com>
2022-03-10 01:19:04 +1100

    Copy change.

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,5 @@
 import Layout from "../../components/Layout/Layout";
 import Container from "../../components/Layout/Container";
-import Link from "next/link";
 
 export default function About() {
     return (
@@ -12,9 +11,7 @@ export default function About() {
                     <a href="https://twitter.com/hrs_andrabi" target="blank">
                         Hassan
                     </a>
-                    . I&apos;m a university student living in Melbourne,
-                    Australia. I built and maintain this website using data from
-                    the{" "}
+                    . I built and maintain CoVstrain using data from the{" "}
                     <a
                         href="https://www.gisaid.org/"
                         target="_blank"
@@ -22,7 +19,10 @@ export default function About() {
                     >
                         GISAID Initiative
                     </a>
-                    .
+                    . CoVstrain uses global genomic sequencing data on
+                    SARS-CoV-2 mutations of interest to visualise mutation
+                    distribution and life cycles over time, and around the
+                    world.
                 </p>
                 <p>
                     If you want to get in touch about this work, you can contact
@@ -36,7 +36,7 @@ export default function About() {
                     </a>
                     , or reach out via{" "}
                     <a href="mailto:hassan.andrabi@unimelb.edu.au">email</a>.
-                    Covstrain is open-source, and accepting contributions via{" "}
+                    CoVstrain is open-source, and accepting contributions via{" "}
                     <a
                         href="https://github.com/HRSAndrabi/covstrain"
                         target="_blank"
@@ -47,7 +47,7 @@ export default function About() {
                 </p>
                 <h4>You may cite this work as:</h4>
                 <div className="not-prose bg-slate-100 font-mono text-sm px-3 py-2">
-                    Hassan R. S. Andrabi. 2022. &quot;Covstrain: Sequencing
+                    Hassan R. S. Andrabi. 2022. &quot;CoVstrain: Sequencing
                     Distributions of SARS-CoV-2 Mutations of Interest.&quot;
                     https://covstrain.com/
                 </div>
