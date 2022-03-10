1a3702943b7a35a8dcaaae078d11d6d78390b315
Hassan Andrabi <handrabi53@gmail.com>
2022-03-10 16:31:01 +1100

    Add GTAG_ID.

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 function MyApp({ Component, pageProps }) {
     // Use the layout defined at the page level, if available
     const tagManagerArgs = {
-        id: process.env.GTAG_ID,
+        gtmId: "G-Q69JNTWRR3",
     };
 
     const getLayout = Component.getLayout || ((page) => page);
