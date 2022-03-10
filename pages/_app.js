08170f4e909458d4e592afb76f4d7fb9b64a7a55
Hassan Andrabi <handrabi53@gmail.com>
2022-03-10 16:06:34 +1100

    Added Google Analytics.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import "../styles/globals.css";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
     // Use the layout defined at the page level, if available
+    const tagManagerArgs = {
+        id: process.env.GTAG_ID,
+    };
+
     const getLayout = Component.getLayout || ((page) => page);
+
+    useEffect(() => {
+        TagManager.initialize(tagManagerArgs);
+    }, []);
 
     return getLayout(<Component {...pageProps} />);
 }
