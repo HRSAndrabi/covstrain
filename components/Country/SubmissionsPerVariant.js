fdda7812d28e238ec3a12a6cf0111d6050a8356b
Hassan Andrabi <handrabi53@gmail.com>
2022-03-12 13:24:38 +1100

    Define VUM.

--- components/Country/SubmissionsPerVariant.js
+++ components/Country/SubmissionsPerVariant.js
@@ -7,8 +7,9 @@ function SubmissionsPerVariant({ plotData, countryData }) {
             <p>
                 The graph below presents the distribution of SARS-CoV-2 variant
                 sequences over time, disaggregated by variants of concern (VOC),
-                and variants of interest (VOI). Untracked variants are
-                classified under the &apos;Other&apos; category.
+                variants of interest (VOI), and variants under monitoring (VUM).
+                Untracked variants are classified under the &apos;Other&apos;
+                category.
             </p>
             <div className="mt-3 mb-8">
                 <div className="text-sm font-medium">
