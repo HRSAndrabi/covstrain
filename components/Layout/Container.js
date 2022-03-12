da7797805d51593f003d295308319e3bd65e5d92
Hassan Andrabi <handrabi53@gmail.com>
2022-03-12 22:45:24 +1100

    Consistent page alignment.

--- components/Layout/Container.js
+++ components/Layout/Container.js
@@ -1,8 +1,8 @@
 const Container = (props) => {
     if (props.prose) {
         return (
-            <div className="px-5 py-10">
-                <article className="max-w-screen-prose mx-auto prose prose-slate prose-sm sm:prose-base mb-10">
+            <div className="px-5">
+                <article className="max-w-screen-prose mx-auto prose prose-slate prose-sm sm:prose-base mb-20">
                     <div className="max-w-screen-lg mx-auto flex flex-col">
                         {props.children}
                     </div>
